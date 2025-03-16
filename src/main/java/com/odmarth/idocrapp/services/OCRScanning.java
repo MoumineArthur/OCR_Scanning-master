2a1208f643ed29014d571f1b475a31d590dd1465
MoumineArthur <oarthurmoumine@gmail.com>
2025-03-16 17:31:36 +0000

    Ajustement

--- src/main/java/com/odmarth/idocrapp/services/OCRScanning.java
+++ src/main/java/com/odmarth/idocrapp/services/OCRScanning.java
@@ -4,6 +4,7 @@ import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
@@ -29,9 +30,36 @@ public class OCRScanning {
 		
 		//tesseract.setPageSegMode(1);
 		tesseract.setOcrEngineMode(1);
-		return tesseract.doOCR(processImageResolution(imageToRead));
+		//return tesseract.doOCR(processImageResolution(imageToRead));
+		return tesseract.doOCR(imageToRead);
 	}
-	
+	 public static BufferedImage changeImageResolution(File inputFile) throws IOException {
+		  int targetDPI =300;
+	        try (FileInputStream inputStream = new FileInputStream(inputFile)) {
+	            BufferedImage image = ImageIO.read(inputStream);
+
+	            if (image == null) {
+	                throw new IOException("Erreur : Impossible de lire l'image !");
+	            }
+
+	            // Calcul du facteur d'échelle basé sur la résolution cible
+	            double scalingFactor = targetDPI / 72.0;  // 72 DPI étant la résolution de base
+	            int newWidth = (int) (image.getWidth() * scalingFactor);
+	            int newHeight = (int) (image.getHeight() * scalingFactor);
+
+	            // Création d'une nouvelle image avec la nouvelle résolution
+	            BufferedImage resizedImage = new BufferedImage(newWidth, newHeight, BufferedImage.TYPE_INT_RGB);
+	            Graphics2D g2d = resizedImage.createGraphics();
+
+	            // Utilisation de l'interpolation bicubique pour une meilleure qualité
+	            g2d.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BICUBIC);
+	            g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+	            g2d.drawImage(image, 0, 0, newWidth, newHeight, null);
+	            g2d.dispose();
+
+	            return resizedImage;
+	        }
+	    }
 	private  static BufferedImage processImageResolution(File orignalImage) throws IOException {
 		BufferedImage image = ImageIO.read(orignalImage);
         
